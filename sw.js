d6b783cd82d8d660ceadc63bedf0ee2cbc7c5623
TonNouveauPseudo <ton-nouvel-email@exemple.com>
2026-07-15 13:35:36 +0000

    ajoute sw

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'lean-cache-v1';
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
